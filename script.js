ad5f43fb401334e9a69161e16805334baa5fa30b
Jhefferson Araujo <jhefferson.araujo@gmail.com>
2024-08-26 11:00:18 -0300

    added spinLogo() function

--- script.js
+++ script.js
@@ -2,3 +2,11 @@ function changeSection (element) {
     document.getElementsByClassName('active')[0].classList.remove('active');
     element.classList.add('active');
 }
+
+function spinLogo () {
+    logo = document.getElementsByClassName('logo')[0];
+    logo.style.animation = 'none';
+    setTimeout(() => {
+        logo.style.animation = 'spin 2.5s';
+    }, 100);
+}
